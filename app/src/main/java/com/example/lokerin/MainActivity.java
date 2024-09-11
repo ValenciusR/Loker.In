941a280bf1a703156317c43009dae544ce9e1ced
DefarioFitriano <defariofitriano@gmail.com>
2024-09-11 23:10:54 +0700

    refactor: naming

--- app/src/main/java/com/example/lokerin/MainActivity.java
+++ app/src/main/java/com/example/lokerin/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.lokerin;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import android.os.Bundle;
@@ -25,13 +24,13 @@ public class MainActivity extends AppCompatActivity {
             Fragment selectedFragment = null;
             int id = item.getItemId();
             if (id == R.id.bottom_home) {
-                    selectedFragment = new HomeFragment();
+                    selectedFragment = new PelangganMyJobFragment();
                     tvPageToolbar.setText("Home");
             } else if (id == R.id.bottom_search) {
-                    selectedFragment = new SearchFragment();
+                    selectedFragment = new PelangganAddJobFragment();
                     tvPageToolbar.setText("Search");
             } else if (id == R.id.bottom_chat) {
-                    selectedFragment = new ChatFragment();
+                    selectedFragment = new PelangganChatFragment();
                     tvPageToolbar.setText("Chat");
             }
             if (selectedFragment != null) {
@@ -45,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         // Load the default fragment (home)
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, new HomeFragment())
+                    .replace(R.id.fragment_container, new PelangganMyJobFragment())
                     .commit();
         }
     }
--- app/src/main/java/com/example/lokerin/PelangganAddJobFragment.java
+++ app/src/main/java/com/example/lokerin/PelangganAddJobFragment.java
@@ -6,12 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HomeFragment extends Fragment {
+public class PelangganAddJobFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_pelanggan_addjob, container, false);
     }
 }
