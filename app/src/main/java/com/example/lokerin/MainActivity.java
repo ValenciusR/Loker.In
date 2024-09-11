5ad9f1e2dd4422b5625ec6553d64f3e906800b2d
DefarioFitriano <defariofitriano@gmail.com>
2024-09-11 21:15:33 +0700

    feat: fix navbar - toolbar

--- app/src/main/java/com/example/lokerin/MainActivity.java
+++ app/src/main/java/com/example/lokerin/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,16 +16,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView btnBackToolbar = findViewById(R.id.btn_back_toolbar);
+        ImageView btnProfileToolbar = findViewById(R.id.btn_back_toolbar);
+        TextView tvPageToolbar = findViewById(R.id.tv_page_toolbar);
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView);
         bottomNavigationView.setOnItemSelectedListener(item -> {
             Fragment selectedFragment = null;
             int id = item.getItemId();
             if (id == R.id.bottom_home) {
                     selectedFragment = new HomeFragment();
+                    tvPageToolbar.setText("Home");
             } else if (id == R.id.bottom_search) {
                     selectedFragment = new SearchFragment();
+                    tvPageToolbar.setText("Search");
             } else if (id == R.id.bottom_chat) {
                     selectedFragment = new ChatFragment();
+                    tvPageToolbar.setText("Chat");
             }
             if (selectedFragment != null) {
                 getSupportFragmentManager().beginTransaction()
