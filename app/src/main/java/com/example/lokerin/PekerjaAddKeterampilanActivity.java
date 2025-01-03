f85e0b3e4f399a9e787954ab117a401363680a45
figomahawangsa <figo.mahawangsa@gmail.com>
2025-01-03 11:54:21 +0700

    feat: fixed add keterampilan activity screen padding

--- app/src/main/java/com/example/lokerin/PekerjaAddKeterampilanActivity.java
+++ app/src/main/java/com/example/lokerin/PekerjaAddKeterampilanActivity.java
@@ -19,6 +19,9 @@ import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -58,6 +61,11 @@ public class PekerjaAddKeterampilanActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pekerja_add_keterampilan);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         btnBack = findViewById(R.id.btn_back_toolbar);
         tvPageTitle = findViewById(R.id.tv_page_toolbar);
